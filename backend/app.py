b2276b074dfc220ba522a0922cfd3e46c9819299
Lucas Ost <lucasost.sm@gmail.com>
2024-06-23 04:16:46 -0300

    Ajuste diretório raiz

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 CORS(app)  # Aplica CORS para todas as rotas e origens
 
+
 DIR_PATH = os.environ.get('DIR_PATH', 'D:\CaminhoArquivoCurso')
 
 @app.route('/')
@@ -15,15 +16,19 @@ def home():
 @app.route('/files')
 def list_files():
     menu = {}
+    root_name = os.path.basename(DIR_PATH)  # Pega apenas o nome do diretório raiz
+    menu[root_name] = {}  # Inicia o menu com o nome do diretório raiz como chave principal
+
     for root, dirs, files in os.walk(DIR_PATH):
         path = root.split(os.sep)
-        sub_menu = menu
-        for folder in path[1:]:  # Começa do 1 para ignorar o diretório raiz na exibição
-            if folder not in sub_menu:
+        sub_menu = menu[root_name]  # Começa a adicionar elementos abaixo do nome do diretório raiz
+        for folder in path[len(DIR_PATH.split(os.sep)):]:  # Ignora o diretório raiz na lista de pastas
+            if folder and folder not in sub_menu:
                 sub_menu[folder] = {}
-            sub_menu = sub_menu[folder]
+            sub_menu = sub_menu.get(folder, {})
         sub_menu['files'] = [os.path.join(root, file) for file in files]
-        sub_menu['MenuId'] = root.replace(DIR_PATH, '').replace(os.sep, '_')
+        sub_menu['MenuId'] = '_' + root.replace(DIR_PATH, '').strip(os.sep).replace(os.sep, '_')
+
     return jsonify(menu)
 
 @app.route('/view/<path:filename>')
